08e6f083970a5baab215b80b859e9f619286cf58
Konstantin Lobanov <lobanov2000k@yandex.ru>
2022-10-18 10:47:42 +0500

    added new style for button

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -1,17 +1,29 @@
 import React, { ReactNode } from 'react'
+import classNames from 'classnames'
 
 import styles from './styles.module.scss'
 
 
 interface Props {
   text: string | ReactNode
-  onClick: () => void
+  onClick: React.MouseEventHandler<HTMLButtonElement> | undefined
   isDisabled?: boolean
+  variant?: 'gray' | 'orange'
+  active?: boolean
 }
 
-const Button = ({ text, onClick, isDisabled }: Props) => {
+const Button = ({ text, onClick, isDisabled, variant = 'orange', active = false }: Props) => {
   return (
-    <button disabled={isDisabled} className={styles['button-root']} onClick={onClick}>
+    <button
+      disabled={isDisabled}
+      className={classNames(
+        styles['button-root'],
+        variant === 'orange' && styles['orange-button'],
+        variant === 'gray' && styles['gray-button'],
+        active && styles['gray-button--active']
+      )}
+      onClick={onClick}
+    >
       {text}
     </button>
   )
